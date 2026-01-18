4b69c7c44ace34df91cbd536ed1beca60d27bbe9
Evgeny Varshavsky <varshavskyevgeny@gmail.com>
2026-01-18 19:31:27 +0300

    Init commit

---
+++ src/test/java/com/github/varshavsky64/quizbattle/QuizBattleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.varshavsky64.quizbattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizBattleBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
